eb9952f05bef828b266f5c82ef6d7c4d2f476af6
Ziqing19 <mzq5176@gmail.com>
2021-12-03 18:41:03 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,20 @@ const fetch = require("node-fetch");
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  res.sendStatus(200);
+  res.status(200).send("This is the fetch server of arkrec");
 });
 
 router.post("/fetch-url", async (req, res) => {
   try {
     if (req.body.url === undefined) return res.sendStatus(400);
     const resRaw = await fetch(req.body.url);
-    if (!resRaw.ok) return res.status(resRaw.status).send(resRaw.statusText);
-    else res.send(await resRaw.text());
+    if (!resRaw.ok) {
+      return res.status(resRaw.status).send(resRaw.statusText);
+    } else {
+      const text = await resRaw.text();
+      console.log(text.length)
+      res.send(text)
+    }
   } catch (err) {
     console.log(err);
     res.sendStatus(400);
