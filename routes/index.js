2ac4abde0d06637fead364847b5315949becb4f4
Ziqing19 <mzq5176@gmail.com>
2021-12-08 13:04:01 -0800

    do some log

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,31 @@ const fetch = require("node-fetch");
 
 /* GET home page. */
 router.get("/", function (req, res) {
+  console.log(
+    new Date(
+    (-480 + new Date().getTimezoneOffset()) * 60000 + new Date().getTime()
+    ).toLocaleString("zh-CN", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    }))
   res.status(200).send("This is the fetch server of arkrec");
 });
 
 router.post("/fetch-url", async (req, res) => {
   try {
     if (req.body.url === undefined) return res.sendStatus(400);
+    console.log(req.body.url);
     const resRaw = await fetch(req.body.url);
     if (!resRaw.ok) {
       return res.status(resRaw.status).send(resRaw.statusText);
     } else {
       const text = await resRaw.text();
-      console.log(text.length)
+      console.log("Res length", text.length)
       res.send(text)
     }
   } catch (err) {
