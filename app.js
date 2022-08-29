3a6ab131b54ed1d748b975bca04c8cca4aad06d5
Ziqing19 <mzq5176@gmail.com>
2022-08-29 12:17:12 -0700

    try fixing cookie parse consume http request body stream 2

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const indexRouter = require("./routes/index");
 const app = express();
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
-app.use('*',  createProxyMiddleware({target: 'https://arkrec.com', secure: false}));app.use(logger("dev"));
+app.use('*',  createProxyMiddleware({target: 'https://arkrec.com', secure: false}));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
