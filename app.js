02a8764dbc4e3ffe9e5a5f56f6c724d297071606
Ziqing19 <mzq5176@gmail.com>
2022-08-29 14:32:03 -0700

    fix proxy 3

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
+
 const app = express();
 
 app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/", indexRouter);
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware, fixRequestBody } = require('http-proxy-middleware');
 app.use(createProxyMiddleware( {
   target: 'https://arkrec.com',
   headers: { "Connection": "keep-alive" },
   secure: false,
   logLevel: "debug",
-  changeOrigin: true
+  changeOrigin: true,
+  onProxyReq: fixRequestBody,
 }));
 
 module.exports = app;
