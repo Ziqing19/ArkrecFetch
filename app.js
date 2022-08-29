d1cc9073eb6b029e41a9a929905852fb846e0a89
Ziqing19 <mzq5176@gmail.com>
2022-08-29 12:32:11 -0700

    try fixing cookie parse consume http request body stream 3

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const querystring = require("querystring");
 
 const indexRouter = require("./routes/index");
 
 const app = express();
-
-const { createProxyMiddleware } = require('http-proxy-middleware');
-app.use('*',  createProxyMiddleware({target: 'https://arkrec.com', secure: false}));
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -16,5 +14,24 @@ app.use(cookieParser());
 
 app.use("/", indexRouter);
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const options = {secure: false}
+options.onProxyReq = (proxyReq, req, res) => {
+  if (!req.body || !Object.keys(req.body).length) {
+    return;
+  }
+  const contentType = proxyReq.getHeader('Content-Type');
+  const writeBody = (bodyData) => {
+    proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+    proxyReq.write(bodyData);
+  };
+  if (contentType === 'application/json') {
+    writeBody(JSON.stringify(req.body));
+  }
+  if (contentType === 'application/x-www-form-urlencoded') {
+    writeBody(querystring.stringify(req.body));
+  }
+}
+app.use('*',  createProxyMiddleware("https://arkrec.com", options));
 
 module.exports = app;
