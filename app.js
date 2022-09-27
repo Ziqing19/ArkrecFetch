a2060183fa4868d643a05b5ae064925e2c6dc007
Ziqing19 <mzq5176@gmail.com>
2022-09-26 21:00:26 -0700

    set json limit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const indexRouter = require("./routes/index");
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
